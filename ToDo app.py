e9d6dfdb15b6357bfee6da2d8e8018a8b7f62294
KhryapovMT <36191973+KhryapovMT@users.noreply.github.com>
2023-03-29 11:59:51 +0300

    Update ToDo app.py

--- ToDo app.py
+++ ToDo app.py
@@ -1,29 +1,34 @@
-tasks = []
+import os
+
+filename = "tasks.txt"
+
+def save_tasks(tasks):
+    with open(filename, "w") as file:
+        for task in tasks:
+            file.write(task + "\n")
+
+if not os.path.exists(filename):
+    with open(filename, "w"):
+        pass
+
+with open(filename, "r") as file:
+    tasks = [line.strip() for line in file.readlines()]
 
 while True:
-    # Displaying the menu
     print("\nTodo app Menu")
     print("1. Add task")
     print("2. View tasks")
     print("3. Mark task as completed")
     print("4. Exit")
-
-    # Getting user input for the menu
     choice = int(input("\nEnter your choice (1-4): "))
-
-    # Adding a task
     if choice == 1:
         task = input("Enter the task: ")
         tasks.append(task)
         print("Task added successfully.")
-
-    # Viewing all the tasks
     elif choice == 2:
         print("\nList of tasks:")
         for index, task in enumerate(tasks):
             print(f"{index+1}. {task}")
-
-    # Marking a task as completed
     elif choice == 3:
         task_number = int(input("Enter the task number to mark as completed: "))
         if task_number <= len(tasks):
@@ -31,12 +36,10 @@ while True:
             tasks.pop(task_number-1)
         else:
             print("Invalid task number.")
-
-    # Exiting the app
     elif choice == 4:
+        save_tasks(tasks)
         print("Thank you for using the Todo app!")
         break
-
-    # Handling invalid input
     else:
         print("Invalid choice. Please try again.")
+
